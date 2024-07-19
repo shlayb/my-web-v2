6b0b010a21dd6f9bda653b3fae72201bda8cef03
shlayb <ayubisal1345@gmail.com>
2024-07-20 00:02:45 +0700

    Initial commit

--- app/Notes/[subject]/page.tsx
+++ app/Notes/[subject]/page.tsx
@@ -24,6 +24,7 @@ export async function generateStaticParams(): Promise<PostPageProps['params'][]>
 }
 
 export default async function SlugPage({ params }: PostPageProps) {
+  generateStaticParams();
   const value = await getPostFromParams(params);
   return (
     <>
